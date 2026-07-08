f21385b7ffaefd5cfabf536343e968b16b202681
markes20 <ssttiillexxx@gmail.com>
2026-07-08 10:31:58 +0500

    first commit

---
+++ src/test/java/com/example/atol_integration_service/AtolIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atol_integration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtolIntegrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
